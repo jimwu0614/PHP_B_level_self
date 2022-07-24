cb402ceff94cbc608cbddb69f7fb1c53bd9912cb
chiachun wu <a0283137@gmail.com>
2022-07-24 22:57:22 +0800

    練習BASE檔

--- quiz1/base.php
+++ quiz1/base.php
@@ -5,10 +5,31 @@ session_start();
 
 
 class DB {
+    public $dsn = "mysql:host=localhost;charset=utf8;dbname=s1110205";
+    public $user = "root";
+    public $pw = "";
+    public $table;
+    public $pdo;
 
-    public function __construct()
+    public function __construct($table)
     {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn,$this->user,$this->pw);
         
+    
+    }
+
+    public function all(...$arg){
+        $sql = "SELECT * FROM $this->table";
+        if (isset($arg[0])) {
+            
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function find($id){
+
     }
 }
 
