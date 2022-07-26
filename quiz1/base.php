799ae5473f6a10865d3c06d5048fa279e24c6ec1
chiachun wu <a0283137@gmail.com>
2022-07-26 23:15:06 +0800

    BASE檔 CRUD結束

--- quiz1/base.php
+++ quiz1/base.php
@@ -1,11 +1,14 @@
 <?php
 
-date_default_timezone_get("Asia/Taipei");
+date_default_timezone_set("Asia/Taipei");
 session_start();
 
 
 class DB {
-    public $dsn = "mysql:host=localhost;charset=utf8;dbname=s1110205";
+    // public $dsn = "mysql:host=localhost;charset=utf8;dbname=s1110205";
+    // public $user = "s1110205";
+    // public $pw = "s1110205";
+    public $dsn = "mysql:host=localhost;charset=utf8;dbname=btest";
     public $user = "root";
     public $pw = "";
     public $table;
@@ -19,18 +22,104 @@ class DB {
     
     }
 
+    // 查
+
     public function all(...$arg){
-        $sql = "SELECT * FROM $this->table";
+        $sql = "SELECT * FROM $this->table ";
+        //如果陣列的第一個值存在
         if (isset($arg[0])) {
-            
+            if(is_array($arg[0])){
+                foreach ($arg[0] as $key => $value) {
+                    $tmp[] = "`$key`='$value'";
+                }
+
+                //     WHERE `id`='1' and `room` = '101'
+                $sql .= " WHERE " . join(" AND ", $tmp);
+            }else{
+                $sql .= $arg[0];
+            }
+
+        //如果陣列有第二個  基本上只會給字串 
+        }elseif(isset($arg[1])){
+            $sql .= $arg[1];
         }
+        
+        echo $sql; 
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
 
     public function find($id){
+        $sql = "SELECT * FROM $this->table ";
+        if (is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[] = " `$key` = '$value' ";
+            }
+            $sql = $sql . " WHERE " . join(" AND ", $tmp);
+            
+        }else{
+            $sql .= " WHERE `id` = '$id' ";
+        }
 
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
+
+
+    public function del($id){
+        $sql = "DELETE  FROM $this->table ";
+        if (is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[] = " `$key` = '$value' ";
+            }
+            $sql = $sql . " WHERE " . join(" AND ", $tmp);
+            
+        }else{
+            $sql .= " WHERE `id` = '$id' ";
+        }
+
+        //會回傳被刪除的筆數  若成功會>=1   若為0表示失敗
+        
+        return $this->pdo->exec($sql);
+        
+    }
+
+
+    public function save($array){
+
+        // 如果有['id']的KEY值  表示是更新  
+        if (isset($array['id'])) {
+            
+            foreach($array as $key => $value){
+                if($key!='id'){
+                    $tmp[] = " `$key` = '$value' ";
+                }
+            }
+            $sql = "UPDATE $this->table SET ". join(' , ',$tmp)." WHERE `id` = '{$array['id']}'";
+            
+        // 如果沒有就是新增
+        }else{
+            $sql = "INSERT INTO $this->table (`".join("`,`",array_keys($array))."`) VALUES ('".join(" ',' ",$array)."')";
+
+        }
+
+
+        //會回傳被刪除的筆數  若成功會>=1   若為0表示失敗
+        
+        return $this->pdo->exec($sql);
+        
+    }
+
+
+
+
+
+}
+
+function dd($array){
+echo "<pre>";
+print_r($array);
+echo "</pre>";
+
 }
 
 
@@ -40,5 +129,12 @@ class DB {
 
 // 刪
 
-// 查
+$Bottom = new DB('b_levelq1_bottom');
+$Ad = 
+
+//$sql = SELECT * FROM B_levelQ1_bottom where `id` = '1'
+// print_r($Bottom->all());                   //Array ( [0] => Array ( [id] => 1 [bottom] => 2022-07-20頁尾版權 ) [1] => Array ( [id] => 2 [bottom] => 2022-07-25 ) )
+print_r($Bottom->all(['id'=>1]));          //Array ( [0] => Array ( [id] => 1 [bottom] => 2022-07-20頁尾版權 ) )
+// print_r($Bottom->all(" where `id` = '1'"));   //Array ( [0] => Array ( [id] => 1 [bottom] => 2022-07-20頁尾版權 ) )
+
 ?>
