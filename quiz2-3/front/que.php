f0332ba0b802681bc48ee30c3fbfebd374b87667
chiachun wu <a0283137@gmail.com>
2022-09-03 15:08:22 +0800

    第三次練習 前台投票製作中

--- quiz2-3/front/que.php
+++ quiz2-3/front/que.php
@@ -9,7 +9,28 @@
             <td class="w10">結果</td>
             <td class="w10">狀態</td>
         </tr>
-
+    <?php
+    $rows = $Que->all(['parent'=>0]);
+    foreach ($rows as $key => $value) {
+    ?>
+        <tr>
+            <td><?=$key+1?>.</td>
+            <td><?=$value['text']?></td>
+            <td><?=$value['total']?></td>
+            <td><a href="?do=result&id=<?=$value['id']?>">結果</a></td>
+            <td>
+    <?php
+    if (isset($_SESSION['user'])) {
+        echo "<a href='?do=vote&id={$value['id']}'>投票</a>";
+    }else{
+        echo "請先登入";
+    }
+    ?>
+            </td>
+        </tr>
+    <?php
+}
+    ?>
         
     </table>
 
